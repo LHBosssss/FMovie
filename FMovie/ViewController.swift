9fcabaf1c1c11208a11d847b8db3cd8a0b04024f
Luong Ho <hoduyluong2108@gmail.com>
2020-04-19 16:01:41 +0700

    Initial Commit

---
+++ FMovie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FMovie
+//
+//  Created by Ho Duy Luong on 4/19/20.
+//  Copyright © 2020 Ho Duy Luong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
